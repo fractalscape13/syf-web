d6d7941c36d927886c2ab51e46d1536ab82ecb3c
Joseph Wangemann <fractalscape13@gmail.com>
2025-12-25 20:52:39 -0700

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? "/syf-web" : "",
+  assetPrefix: isProd ? "/syf-web/" : "",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
